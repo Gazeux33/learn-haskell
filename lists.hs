7f733d1827554b58f5a49c24f4400f054bf49b47
tcastillo-ubuntu <theocastillo@yahoo.com>
2025-02-13 21:17:42 +0100

    reverse + zip

--- lists.hs
+++ lists.hs
@@ -11,9 +11,14 @@ main = do
   --putStrLn $ show $ filter2 odd l
   --putStrLn $ show $ filter3 odd l
 
-  putStrLn $ show $ foldl' (+) 0 l
-  putStrLn $ show $ foldr' (+) 0 l
-  putStrLn $ show $ foldl1' (+) l
+  --putStrLn $ show $ foldl' (+) 0 l
+  --putStrLn $ show $ foldr' (+) 0 l
+  --putStrLn $ show $ foldl1' (+) l
+
+  --putStrLn $ show $ reverse1 l
+  --putStrLn $ show $ reverse2 l
+
+  putStrLn $ show $ zip1 l  $ reverse1 l
 
 
 
@@ -25,7 +30,7 @@ map1 _ [] = []
 map1 f (x:xs) = f x : map1 f xs
 
 map2 :: (a -> b) -> [a] -> [b]
-map2 f xs = foldr (\x acc -> f x : acc ) [] xs
+map2 f = foldr (\x acc -> f x : acc ) []
 
 map3 :: (a -> b) -> [a] -> [b]
 map3 f xs = [f x | x <- xs]
@@ -37,7 +42,7 @@ filter1 _ [] = []
 filter1 f (x:xs) = if f x then x:filter1 f xs else filter1 f xs
 
 filter2 :: (a -> Bool) -> [a] -> [a]
-filter2 f xs = foldr (\x acc -> if f x then x:acc else acc) [] xs
+filter2 f  = foldr (\x acc -> if f x then x:acc else acc) []
 
 filter3 :: (a -> Bool) -> [a] -> [a]
 filter3 f xs = [x | x <- xs , f x]
@@ -56,6 +61,23 @@ foldr' f acc (x:xs) = f x (foldr' f acc xs)
 
 foldl1' :: (a -> a -> a) -> [a] -> a
 foldl1' f (x:xs) = foldl f x xs
+
+
+--reverse :: [a] -> [a] : Inverse une liste.
+reverse1 :: [a] -> [a]
+reverse1 = foldl (\acc x-> x:acc) []
+
+reverse2 :: [a] -> [a]
+reverse2 [] = []
+reverse2 (x:xs) = reverse2 xs ++ [x]
+
+--zip :: [a] -> [b] -> [(a, b)] : Associe les éléments de deux listes.
+zip1 :: [a] -> [b] -> [(a, b)]
+zip1 _ [] = []
+zip1 [] _ = []
+zip1 (x:xs) (y:ys) = (x,y):zip1 xs ys 
+
+
 
 
 
