bca16e7abe97bcd5f14a01b99829ed07a3fc6ccb
Theo <thoecastillo@yahoo.com>
2025-02-15 15:04:59 +0100

    add mathematics module with placeholder functions for common operations

---
+++ mathematics.hs
@@ -0,0 +1,28 @@
+
+
+main :: IO ()
+main = do
+  let l = [1,2,3,4,5,6]
+
+
+
+
+-- gcd :: Int -> Int -> Int : Calcul du plus grand commun diviseur.
+
+--lcm :: Int -> Int -> Int : Calcul du plus petit commun multiple.
+
+--factorial :: Int -> Int : Calcul de la factorielle d'un entier.
+
+
+
+--fibonacci :: Int -> Int : Calcul de la suite de Fibonacci.
+
+--primeFactors :: Int -> [Int] : Décomposition en facteurs premiers.
+
+--isPrime :: Int -> Bool : Vérifie si un nombre est premier.
+
+--sum :: Num a => [a] -> a : Somme d'une liste.
+
+--product :: Num a => [a] -> a : Produit d'une liste.
+
+--mean :: (Fractional a) => [a] -> a : Moyenne d'une liste de nombres.
